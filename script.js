36f71f6627ef78a610a0bf8ddd5cfd12e7a52658
Lucas <lrigon2904@gmail.com>
2022-06-13 14:47:50 -0300

    finalização

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome =  "jhon";
+let sobrenome = "doyle";
+let anoNasciemnto = 1995;
+let cidade = "Curitiba";
+let ano = 2022;
+
+
+console.log("Eu sou", nome,sobrenome ,"tenho anos", ano - anoNasciemnto, "anos e moro em", cidade)
+console.log("Eu sou", nome,sobrenome ,"tenho anos", 2022 - anoNasciemnto, "anos e moro em", cidade)
